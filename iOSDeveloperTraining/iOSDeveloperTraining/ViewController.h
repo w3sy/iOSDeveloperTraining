e2800e251ec2904c22caa22f43055fd64d01d95d
王博 <w3sy@qq.com>
2015-11-22 12:40:29 +0800

    Initialization project

---
+++ iOSDeveloperTraining/iOSDeveloperTraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDeveloperTraining
+//
+//  Created by 王博 on 15/11/22.
+//  Copyright © 2015年 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
