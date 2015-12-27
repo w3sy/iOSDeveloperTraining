99f18b70c11a5dbe1ba0ef88d8e906fe6403af85
王博 <w3sy@qq.com>
2015-12-27 18:39:22 +0800

    fix storyboard layout bug

--- iOSDeveloperTraining/Pods/CBStoreHouseRefreshControl/Class/CBStoreHouseRefreshControl.m
+++ iOSDeveloperTraining/Pods/CBStoreHouseRefreshControl/Class/CBStoreHouseRefreshControl.m
@@ -135,7 +135,7 @@ NSString *const yKey = @"y";
 
 - (void)scrollViewDidScroll
 {
-    //if (self.originalTopContentInset == 0) self.originalTopContentInset = self.scrollView.contentInset.top;
+    if (self.originalTopContentInset == 0) self.originalTopContentInset = self.scrollView.contentInset.top;
     self.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, self.realContentOffsetY*krelativeHeightFactor);
     if (self.state == CBStoreHouseRefreshControlStateIdle)
         [self updateBarItemsWithProgress:self.animationProgress];
