645ff9385c233153620327a50773b6be65073692
王博 <w3sy@hotmail.com>
2016-10-31 11:20:26 +0800

    fix [super awakeFromNib] missing call

--- iOSDeveloperTraining/iOSDeveloperTraining/RssNewsTableViewCell.m
+++ iOSDeveloperTraining/iOSDeveloperTraining/RssNewsTableViewCell.m
@@ -10,10 +10,6 @@
 
 @implementation RssNewsTableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
